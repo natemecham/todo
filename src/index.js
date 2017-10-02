5c6811043b4020d471ee0ce8109da61e38c33fee
Nate Mecham <enemecham@gmail.com>
2017-10-02 18:23:13 -0400

    styles and delete function

--- src/index.js
+++ src/index.js
@@ -4,19 +4,29 @@ import './index.css';
 
 
 const TodoListItem = (props) => {
-	const {isComplete,id,onChange,text} = props;
+	const {isComplete,id,onChange,text,handleDropDown,isMenuOpen,onDelete} = props;
 	const isCompleteClass = isComplete ? 'complete' : 'incomplete';
+	const isMenuOpenClass = isMenuOpen ? 'open' : 'closed';
 	
 	return(<li className={ isCompleteClass + ' todo_item'}>	
-				<input type="checkbox" checked={isComplete} value={id} onChange={onChange}/>
-				{text}
+				
+				<input type="checkbox" checked={isComplete} value={id} id={"list_"+id} onChange={onChange}/>
+				<label for={"list_"+id}>
+					{text}
+				</label>
+				<div className={isMenuOpenClass + ' drop_down'}>
+					<button className="toggle_drop"  onClick={handleDropDown}></button>
+					<div className="menu">
+						<button onClick={onDelete} className="delete btn">Delete</button>
+					</div>
+				</div>
 			</li>
 		);
 			
 }//end TodoListItem
 
 const TodoList = (props) => {
-	const {list,onChange,show} = props;	
+	const {list,onChange,show,handleDropDown,isMenuOpen,onDelete} = props;	
 			
 	return(
 		<ul>
@@ -33,7 +43,7 @@ const TodoList = (props) => {
 						return false;
 				}
 			}).map((data,index) => {
-				return <TodoListItem key={index} id={index} text={data.text} isComplete={data.isComplete} onChange={ () => onChange(index)}/>;
+				return <TodoListItem key={index} id={index} text={data.text} isComplete={data.isComplete} onChange={ () => onChange(index)} handleDropDown={() => handleDropDown(index) } isMenuOpen={data.isMenuOpen} onDelete={ () => onDelete(index)} />;
 				})
 			}
 		</ul>);
@@ -45,7 +55,7 @@ const Input = (props) => {
 	return(
 		<div className="form_wrapper" onSubmit={handleSubmit}>
 			<form name="new_todo">
-				<input type="text" value={input} onChange={handleChange} />
+				<input placeholder="What do you need to do?" type="text" value={input} onChange={handleChange} />
 				<input type="submit" value="Add" />
 			</form>
 		</div>
@@ -59,14 +69,20 @@ function Toggle(props){
 		return sum + (!todo.isComplete ? 1 : 0);
 	},0);
 	
+	const completeTodoCount = todo.reduce( (sum,todo ) => {
+		return sum + (todo.isComplete ? 1 : 0);
+	},0);
+	
+	const allTodoCount = todo.length;
+	
 	
 	return(
 		<div className="toggle">
-			<button className={props.show === 'all' ? ' on ' : ''} onClick={() => onClick('all')}>All</button>
-			<button className={props.show === 'active' ? ' on ' : ''} onClick={() => onClick('active')}>Active</button>
-			<button className={props.show === 'complete' ? ' on ' : ''} onClick={() => onClick('complete')}>Complete</button>
-			<button onClick={onClearClick}>Clear Complete</button>
-			<p>Left To-Do:{activeTodoCount}</p>
+			<button className={props.show === 'all' ? ' on ' : ''} onClick={() => onClick('all')}>All <span>{allTodoCount}</span></button>
+			<button className={props.show === 'active' ? ' on ' : ''} onClick={() => onClick('active')}>Active <span>{activeTodoCount}</span></button>
+			<button className={props.show === 'complete' ? ' on ' : ''} onClick={() => onClick('complete')}>Complete <span>{completeTodoCount}</span></button>
+			<button className="clear" onClick={onClearClick}>Clear Complete</button>
+			
 		</div>	
 	);
 }
@@ -77,7 +93,7 @@ class App extends React.Component {
 		this.state = {
 			input: '',
 			todo: new Array(0),
-			show:'all'	
+			show:'all',	
 		}
 		
 		//this.handleChange = this.handleChange.bind(this);
@@ -104,6 +120,7 @@ class App extends React.Component {
 			todo_list[todo_list.length] = {
 				text: add_value,
 				isComplete: false,
+				isMenuOpen:false
 			};
 			this.setState({todo:todo_list});
 		}
@@ -121,21 +138,29 @@ class App extends React.Component {
 	
 	handleClearClick = () => {
 		const new_todo = this.state.todo.slice();
-		for(let i=new_todo.length - 1; i >= 0 ; i--){
-			if(new_todo[i].isComplete){
-				new_todo.splice(i,1);
-			}
-		}
+		const cleared_complete = new_todo.filter(todo => !todo.isComplete);
 		
+		this.setState({todo:cleared_complete});
+	}
+	
+	handleDropDown = (i) => {
+		const new_todo = this.state.todo.slice();
+		new_todo[i].isMenuOpen = !new_todo[i].isMenuOpen;
 		this.setState({todo:new_todo});
+	}
+	
+	handleDelete = (i) => {
+		const new_todo = this.state.todo.slice();
+		new_todo.splice(i,1);
+		this.setState({todo:new_todo});
+
 	}
 	
 	render(){
 		return(
 			<div className="todo_wrapper">
-				<h1>Do These Things</h1>
 				<Input input={this.state.input} handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
-				<TodoList list={this.state.todo} show={this.state.show} onChange={(i)=>this.handleCheck(i)}/>
+				<TodoList list={this.state.todo} show={this.state.show} onChange={(i)=>this.handleCheck(i)} handleDropDown={(i)=>this.handleDropDown(i)} onDelete={(i) => this.handleDelete(i)}/>
 				<Toggle	onClick={(i) => this.handleShow(i)} show={this.state.show} onClearClick={this.handleClearClick} todo={this.state.todo}/>
 			</div>
 		);	
