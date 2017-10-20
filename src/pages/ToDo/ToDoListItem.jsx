f10f9ee4d2f88e07167f956770933b36e963a76a
Xiaopow <xiaoyang88@gmail.com>
2017-10-20 10:53:31 +0800

    move dropdown state to item component

--- src/pages/ToDo/ToDoListItem.jsx
+++ src/pages/ToDo/ToDoListItem.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class ToDoListItem extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      open: false,
+    }
+  }
+
   componentWillReceiveProps(nextProps) {
     if (!this.props.isComplete && nextProps.isComplete) {
       window.requestAnimationFrame(() => {
@@ -14,10 +22,14 @@ class ToDoListItem extends React.Component {
     }
   }
 
+  handleDropDown = () => {
+    this.setState({ open: !this.state.open });
+  }
+
   render() {
-    const {isComplete,id,onChange,text,handleDropDown,isMenuOpen,onDelete,flip} = this.props;
+    const {isComplete, id, onChange, text, isMenuOpen, onDelete, flip} = this.props;
     const isCompleteClass = isComplete ? 'complete' : 'incomplete';
-    const isMenuOpenClass = isMenuOpen ? 'open' : 'closed';
+    const isMenuOpenClass = this.state.open ? 'open' : 'closed';
 
     return(
       <li className={ isCompleteClass + ' todo_item'}>
@@ -26,7 +38,7 @@ class ToDoListItem extends React.Component {
           {text}
         </label>
         <div className={isMenuOpenClass + ' drop_down'}>
-          <button className="toggle_drop"  onClick={handleDropDown}></button>
+          <button className="toggle_drop" onClick={this.handleDropDown}></button>
           <div className="menu">
             <Link className="btn" to={`/task/${id}`}>See Details</Link>
             <button onClick={onDelete} className="delete btn">Delete</button>
